25760cc9376167c26fee272c42a1486620cfde0a
TrueCat17 <truecat17@gmail.com>
2019-12-25 14:15:33 +0300

    upd bins, add arch. linux-x86_64

--- start.sh
+++ start.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=./linux-i686/
-./linux-i686/Ren-Engine
+
+if [ "`uname -s`" = "Darwin" ]; then
+	echo "MacOS is not supported now"
+	exit 1
+fi
+
+case "`uname -m`" in
+	x86_64|amd64)
+		./Ren-Engine/linux-x86_64
+		;;
+	i*86)
+		./Ren-Engine/linux-i686
+		;;
+	*)
+		echo "Unknown OS"
+		echo "You can run need file in dir Ren-Engine"
+		exit 1
+		;;
+esac
