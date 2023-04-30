a332132e40376c619b32c6041957673e80242a21
TrueCat17 <truecat17@gmail.com>
2023-04-30 13:30:14 +0300

    hotfix for start.exe

--- win_start/build.sh
+++ win_start/build.sh
@@ -4,4 +4,4 @@ set -e
 CC=i686-w64-mingw32-gcc
 
 $CC -Os -c main.c -o main.o
-$CC -s -static -static-libgcc main.o -o start.exe
+$CC -s -mwindows -static -static-libgcc main.o -o start.exe # -mwindows = dont show console
