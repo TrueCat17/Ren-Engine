e063f1dcf97501ba8716e9e57522792366729cca
TrueCat17 <truecat17@gmail.com>
2024-09-30 19:39:00 +0300

    start-file for win32 starts Ren-Engine with abs-path now

--- win_start/main.c
+++ win_start/main.c
@@ -1,3 +1,4 @@
+#include <libloaderapi.h> //GetModuleFileNameA
 #include <dirent.h>
 #include <errno.h>
 #include <locale.h>
@@ -12,17 +13,33 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	
+	const size_t BUFFER_SIZE = 300;
+	
 	const char *RE_LANG = getenv("RE_LANG");
 	if (!RE_LANG || RE_LANG[0] == 0) {
 		setlocale(LC_ALL, "");
 		const char *locale = setlocale(LC_ALL, NULL);
 		
-		char envVar[300];
-		snprintf(envVar, 300, "RE_LANG=%s", locale);
+		char envVar[BUFFER_SIZE];
+		snprintf(envVar, BUFFER_SIZE, "RE_LANG=%s", locale);
 		putenv(envVar);
 	}
 	
-	const char *dirPath = "./Ren-Engine";
+	char path[BUFFER_SIZE];
+	size_t size = GetModuleFileNameA(NULL, path, BUFFER_SIZE);
+	size_t lastSep = 0;
+	for (size_t i = 0; i < size; ++i) {
+		if (path[i] == '\\') {
+			path[i] = '/';
+		}
+		if (path[i] == '/') {
+			lastSep = i;
+		}
+	}
+	path[lastSep] = 0;
+	
+	char dirPath[BUFFER_SIZE];
+	snprintf(dirPath, BUFFER_SIZE, "%s/Ren-Engine", path);
 	
 	DIR *dir = opendir(dirPath);
 	if (!dir) {
@@ -52,8 +69,8 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	
-	char exePath[300];
-	snprintf(exePath, 300, "%s/%s", dirPath, fileName);
+	char exePath[BUFFER_SIZE];
+	snprintf(exePath, BUFFER_SIZE, "%s/%s", dirPath, fileName);
 	
 	if (execl(exePath, "Ren-Engine", NULL)) {
 		printf("Error on execl <%s>: %s\n", exePath, strerror(errno));
